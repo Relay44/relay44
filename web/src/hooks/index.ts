c78c4ecb6b7faaacf2662df6e73083fa74d9ef2b
0p3r4t0r44 <hello@relay44.com>
2025-12-22 13:16:29 +0100

    add index component and hook

---
+++ web/src/hooks/index.ts
@@ -0,0 +1,35 @@
+export { useMarkets, useMarket, useOrderBook, useTrades } from './useMarkets';
+export { useOrders, useOrder, usePlaceOrder, useCancelOrder } from './useOrders';
+export { usePositions, usePosition, useClaimWinnings } from './usePositions';
+export {
+  useAgents,
+  useAgent,
+  useCreateAgent,
+  useExecuteAgent,
+  useExternalAgents,
+  useCreateExternalAgent,
+  useExecuteExternalAgent,
+} from './useAgents';
+export { useAuth } from './useAuth';
+export {
+  useDecisionCells,
+  useDecisionCell,
+  useCreateDecisionCell,
+  useUpdateDecisionCell,
+  useAddDecisionAction,
+  useAddDecisionNode,
+  useUpdateDecisionNode,
+  useAttachDecisionMarket,
+  useAttachDecisionAgent,
+  useRecalculateDecisionCell,
+  useUpdateDecisionAutomation,
+  useUpsertDecisionAlert,
+} from './useDecisions';
+export { useRuntimeMode } from './useRuntimeMode';
+export { useSessionState } from './useSessionState';
+export {
+  useWebSocket,
+  useOrderBookSubscription,
+  useTradeSubscription,
+  usePriceSubscription,
+} from './useWebSocket';
