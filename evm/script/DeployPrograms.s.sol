16f7e1a2250d25b437e00de36786727f7f5702f8
kael <kael@relay44.com>
2026-01-19 17:32:35 +0100

    add eployrograms.s deploy script

---
+++ evm/script/DeployPrograms.s.sol
@@ -0,0 +1,189 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {MarketCore} from "../src/MarketCore.sol";
+import {OrderBook} from "../src/OrderBook.sol";
+import {CollateralVault} from "../src/CollateralVault.sol";
+import {AgentRuntime} from "../src/AgentRuntime.sol";
+import {AgentIdentityRegistry} from "../src/AgentIdentityRegistry.sol";
+import {AgentReputationRegistry} from "../src/AgentReputationRegistry.sol";
+import {ERC8004IdentityRegistry} from "../src/ERC8004IdentityRegistry.sol";
+import {ERC8004ReputationRegistry} from "../src/ERC8004ReputationRegistry.sol";
+import {ERC8004ValidationRegistry} from "../src/ERC8004ValidationRegistry.sol";
+
+interface IAccessControlLike {
+    function grantRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+    function hasRole(bytes32 role, address account) external view returns (bool);
+}
+
+contract DeployProgramsScript is Script {
+    error MissingAdmin();
+    error MissingCollateralToken();
+    error ZeroAddress(string field);
+
+    function run() external {
+        address admin = _envAddressOr("BASE_ADMIN", address(0));
+        if (admin == address(0)) revert MissingAdmin();
+
+        address bootstrapAdmin = _envAddressOr("BOOTSTRAP_ADMIN", admin);
+        if (bootstrapAdmin == address(0)) revert ZeroAddress("BOOTSTRAP_ADMIN");
+
+        address pauser = _envAddressOr("BASE_PAUSER", admin);
+        address resolver = _envAddressOr("BASE_RESOLVER", admin);
+        address marketCreator = _envAddressOr("BASE_MARKET_CREATOR", admin);
+        address operator = _envAddressOr("BASE_OPERATOR", admin);
+        address runtimeOperator = _envAddressOr("BASE_AGENT_RUNTIME_OPERATOR", address(0));
+        address reputationOracle = _envAddressOr("BASE_REPUTATION_ORACLE", operator);
+        address erc8004Issuer = _envAddressOr("BASE_IDENTITY_ISSUER", admin);
+        address erc8004Attester = _envAddressOr("BASE_REPUTATION_ATTESTER", reputationOracle);
+        address erc8004ValidationManager = _envAddressOr("BASE_VALIDATION_MANAGER", admin);
+        address erc8004Validator = _envAddressOr("BASE_VALIDATION_VALIDATOR", erc8004Attester);
+
+        address collateralToken = _resolveCollateralToken();
+        if (collateralToken == address(0)) revert MissingCollateralToken();
+
+        vm.startBroadcast();
+
+        MarketCore marketCore = new MarketCore(bootstrapAdmin);
+        CollateralVault collateralVault = new CollateralVault(bootstrapAdmin, collateralToken);
+        OrderBook orderBook = new OrderBook(bootstrapAdmin, address(marketCore), address(collateralVault));
+        AgentRuntime agentRuntime = new AgentRuntime(bootstrapAdmin, address(orderBook));
+        AgentIdentityRegistry identityRegistry = new AgentIdentityRegistry(bootstrapAdmin);
+        AgentReputationRegistry reputationRegistry =
+            new AgentReputationRegistry(bootstrapAdmin, address(identityRegistry));
+        ERC8004IdentityRegistry erc8004IdentityRegistry = new ERC8004IdentityRegistry(bootstrapAdmin);
+        ERC8004ReputationRegistry erc8004ReputationRegistry =
+            new ERC8004ReputationRegistry(bootstrapAdmin, address(erc8004IdentityRegistry));
+        ERC8004ValidationRegistry erc8004ValidationRegistry =
+            new ERC8004ValidationRegistry(bootstrapAdmin, address(erc8004IdentityRegistry));
+
+        _configureMarketCore(marketCore, bootstrapAdmin, admin, marketCreator, resolver, pauser);
+        _configureCollateralVault(collateralVault, bootstrapAdmin, admin, operator, pauser, address(orderBook));
+        _configureOrderBook(orderBook, bootstrapAdmin, admin, pauser, address(agentRuntime), runtimeOperator);
+        _configureIdentityRegistry(identityRegistry, bootstrapAdmin, admin, pauser, address(agentRuntime));
+        _configureReputationRegistry(reputationRegistry, bootstrapAdmin, admin, pauser, reputationOracle);
+        _configureErc8004IdentityRegistry(erc8004IdentityRegistry, bootstrapAdmin, admin, pauser, erc8004Issuer);
+        _configureErc8004ReputationRegistry(erc8004ReputationRegistry, bootstrapAdmin, admin, pauser, erc8004Attester);
+        _configureErc8004ValidationRegistry(
+            erc8004ValidationRegistry,
+            bootstrapAdmin,
+            admin,
+            pauser,
+            erc8004ValidationManager,
+            erc8004Validator
+        );
+        _configureAgentRuntime(agentRuntime, bootstrapAdmin, admin, pauser, address(identityRegistry));
+
+        vm.stopBroadcast();
+
+        console2.log("chainId:", block.chainid);
+        console2.log("admin:", admin);
+        console2.log("bootstrapAdmin:", bootstrapAdmin);
+        console2.log("MarketCore:", address(marketCore));
+        console2.log("CollateralVault:", address(collateralVault));
+        console2.log("OrderBook:", address(orderBook));
+        console2.log("AgentRuntime:", address(agentRuntime));
+        console2.log("AgentIdentityRegistry:", address(identityRegistry));
+        console2.log("AgentReputationRegistry:", address(reputationRegistry));
+        console2.log("ERC8004IdentityRegistry:", address(erc8004IdentityRegistry));
+        console2.log("ERC8004ReputationRegistry:", address(erc8004ReputationRegistry));
+        console2.log("ERC8004ValidationRegistry:", address(erc8004ValidationRegistry));
+        console2.log("collateralToken:", collateralToken);
+        console2.log("marketCreator:", marketCreator);
+        console2.log("resolver:", resolver);
+        console2.log("operator:", operator);
+        console2.log("pauser:", pauser);
+        console2.log("runtimeOperator:", runtimeOperator);
+        console2.log("reputationOracle:", reputationOracle);
+        console2.log("erc8004Issuer:", erc8004Issuer);
+        console2.log("erc8004Attester:", erc8004Attester);
+        console2.log("erc8004ValidationManager:", erc8004ValidationManager);
+        console2.log("erc8004Validator:", erc8004Validator);
+    }
+
+    function _configureMarketCore(
+        MarketCore marketCore,
+        address bootstrapAdmin,
+        address admin,
+        address marketCreator,
+        address resolver,
+        address pauser
+    ) internal {
+        bytes32 defaultAdminRole = marketCore.DEFAULT_ADMIN_ROLE();
+
+        _grantRoleIfMissing(IAccessControlLike(address(marketCore)), marketCore.MARKET_CREATOR_ROLE(), marketCreator);
+        _grantRoleIfMissing(IAccessControlLike(address(marketCore)), marketCore.RESOLVER_ROLE(), resolver);
+        _grantRoleIfMissing(IAccessControlLike(address(marketCore)), marketCore.PAUSER_ROLE(), pauser);
+
+        if (bootstrapAdmin != admin) {
+            _grantRoleIfMissing(IAccessControlLike(address(marketCore)), defaultAdminRole, admin);
+            _revokeRoleIfPresent(
+                IAccessControlLike(address(marketCore)), marketCore.MARKET_CREATOR_ROLE(), bootstrapAdmin
+            );
+            _revokeRoleIfPresent(IAccessControlLike(address(marketCore)), marketCore.RESOLVER_ROLE(), bootstrapAdmin);
+            _revokeRoleIfPresent(IAccessControlLike(address(marketCore)), marketCore.PAUSER_ROLE(), bootstrapAdmin);
+            _revokeRoleIfPresent(IAccessControlLike(address(marketCore)), defaultAdminRole, bootstrapAdmin);
+        }
+    }
+
+    function _configureOrderBook(
+        OrderBook orderBook,
+        address bootstrapAdmin,
+        address admin,
+        address pauser,
+        address agentRuntime,
+        address runtimeOperator
+    ) internal {
+        bytes32 defaultAdminRole = orderBook.DEFAULT_ADMIN_ROLE();
+
+        _grantRoleIfMissing(IAccessControlLike(address(orderBook)), orderBook.PAUSER_ROLE(), pauser);
+        _grantRoleIfMissing(IAccessControlLike(address(orderBook)), orderBook.AGENT_RUNTIME_ROLE(), agentRuntime);
+        if (runtimeOperator != address(0)) {
+            _grantRoleIfMissing(IAccessControlLike(address(orderBook)), orderBook.AGENT_RUNTIME_ROLE(), runtimeOperator);
+        }
+
+        if (bootstrapAdmin != admin) {
+            _grantRoleIfMissing(IAccessControlLike(address(orderBook)), defaultAdminRole, admin);
+            _revokeRoleIfPresent(IAccessControlLike(address(orderBook)), orderBook.PAUSER_ROLE(), bootstrapAdmin);
+            _revokeRoleIfPresent(IAccessControlLike(address(orderBook)), orderBook.AGENT_RUNTIME_ROLE(), bootstrapAdmin);
+            _revokeRoleIfPresent(IAccessControlLike(address(orderBook)), defaultAdminRole, bootstrapAdmin);
+        }
+    }
+
+    function _configureCollateralVault(
+        CollateralVault collateralVault,
+        address bootstrapAdmin,
+        address admin,
+        address operator,
+        address pauser,
+        address orderBook
+    ) internal {
+        bytes32 defaultAdminRole = collateralVault.DEFAULT_ADMIN_ROLE();
+
+        _grantRoleIfMissing(IAccessControlLike(address(collateralVault)), collateralVault.OPERATOR_ROLE(), operator);
+        _grantRoleIfMissing(IAccessControlLike(address(collateralVault)), collateralVault.OPERATOR_ROLE(), orderBook);
+        _grantRoleIfMissing(IAccessControlLike(address(collateralVault)), collateralVault.PAUSER_ROLE(), pauser);
+
+        if (bootstrapAdmin != admin) {
+            _grantRoleIfMissing(IAccessControlLike(address(collateralVault)), defaultAdminRole, admin);
+            _revokeRoleIfPresent(
+                IAccessControlLike(address(collateralVault)), collateralVault.OPERATOR_ROLE(), bootstrapAdmin
+            );
+            _revokeRoleIfPresent(
+                IAccessControlLike(address(collateralVault)), collateralVault.PAUSER_ROLE(), bootstrapAdmin
+            );
+            _revokeRoleIfPresent(IAccessControlLike(address(collateralVault)), defaultAdminRole, bootstrapAdmin);
+        }
+    }
+
+    function _configureAgentRuntime(
+        AgentRuntime agentRuntime,
+        address bootstrapAdmin,
+        address admin,
+        address pauser,
+        address identityRegistry
+    ) internal {
+        bytes32 defaultAdminRole = agentRuntime.DEFAULT_ADMIN_ROLE();
+
